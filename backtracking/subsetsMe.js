dbe18a712ce468ffb5292abd70ccd9ea06db2cc1
Mohamad-Ilham-Ramadhan <mohamadilhamramadhan@gmail.com>
2023-05-05 15:29:15 +0700

    edit prev's descriptions

--- backtracking/subsetsMe.js
+++ backtracking/subsetsMe.js
@@ -20,6 +20,8 @@
       -10 <= nums[i] <= 10
       All the numbers of nums are unique.
 
+   Solution Idea:
+
    Power Set: Power set P(S) of a set S is the set of all subsets of S. For example S = {a, b, c} then P(s) = {{}, {a}, {b}, {c}, {a,b}, {a, c}, {b, c}, {a, b, c}}.
    If S has n elements in it then P(s) will have 2n elements
 
