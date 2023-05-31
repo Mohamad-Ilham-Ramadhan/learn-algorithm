e8263c76dba3a1ceecdbb35c811601869ae2d168
Mohamad-Ilham-Ramadhan <mohamadilhamramadhan@gmail.com>
2023-05-31 19:55:37 +0700

    (graph) leetcode: clone graph (medium), solution by myself using BFS, hashmap. Good Runtime

---
+++ graph/cloneGraph_Me.js
@@ -0,0 +1,217 @@
+/*
+  133. Clone Graph (medium)
+
+  Given a reference of a node in a connected undirected graph.
+
+  Return a deep copy (clone) of the graph.
+
+  Each node in the graph contains a value (int) and a list (List[Node]) of its neighbors.
+
+  class Node {
+      public int val;
+      public List<Node> neighbors;
+  }
+  
+
+  Test case format:
+
+  For simplicity, each node's value is the same as the node's index (1-indexed). For example, the first node with `val == 1`, the second node with `val == 2`, and so on. The graph is represented in the test case using an adjacency list.
+
+  An adjacency list is a collection of unordered lists used to represent a finite graph. Each list describes the set of neighbors of a node in the graph.
+
+  The given node will always be the first node with `val = 1`. You must return the copy of the given node as a reference to the cloned graph.
+
+  
+
+  Example 1:
+                          1--------------------2
+                          |   you can't return |
+               |-------x  |    the same graph  |
+               |          4--------------------3
+               |
+    1--------------------2                1-----------------------------2
+    |    original        |   -------->    |    This looks like a        |
+    |     graph          |                |    clone. The nodes are     |
+    4--------------------3                |  New. Graph looks the same  |
+              |                           4-----------------------------3
+              |
+              |             1---------------------------3              
+              |             |   The nodes were          |
+              |--------x    |   cloned. But the graph   |
+                            |   is messed up. Doesn't   |
+                            |    have same connections. |         
+                            4---------------------------2
+
+    Input: adjList = [[2,4],[1,3],[2,4],[1,3]]
+    Output: [[2,4],[1,3],[2,4],[1,3]]
+    Explanation: There are 4 nodes in the graph.
+      1st node (val = 1)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+      2nd node (val = 2)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
+      3rd node (val = 3)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+      4th node (val = 4)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
+
+  Example 2:
+
+
+    Input: adjList = [[]]
+    Output: [[]]
+    Explanation: Note that the input contains one empty list. The graph consists of only one node with val = 1 and it does not have any neighbors.
+
+  Example 3:
+    Input: adjList = []
+    Output: []
+    Explanation: This an empty graph, it does not have any nodes.
+  
+
+  Constraints:
+    - The number of nodes in the graph is in the range [0, 100].
+    - 1 <= Node.val <= 100
+    - Node.val is unique for each node.
+    - There are no repeated edges and no self-loops in the graph.
+    - The Graph is connected and all nodes can be visited starting from the given node.
+
+  Solution by myself:
+    using bfs -> deque 
+    hashmap to store all new created nodes reference 
+  
+
+  LeetCode submission: 
+    Runtime: 64 ms, beats 48.9%
+    Memory: 44.2 MB, beats 23.21%
+*/
+
+// Definition for a Node.
+class Node {
+   constructor(val, neighbors) {
+     this.val = val === undefined ? 0 : val;
+     this.neighbors = neighbors === undefined ? [] : neighbors;
+   }
+ };
+ 
+ // self made deque
+ class Deque {
+   constructor() {
+     this.items = {};
+     this.frontIndex = 0;
+     this.endIndex = 1;
+     this.size = 0;
+   }
+ 
+   // Add an element to the front of the deque
+   addFront(element) {
+     this.items[this.frontIndex] = element;
+     // this.endIndex = this.frontIndex === 0 && this.endIndex === 0 ? 1 : this.endIndex;
+     this.frontIndex--;
+     this.size++;
+   }
+ 
+   // Add an element to the rear of the deque
+   addRear(element) {
+     this.items[this.endIndex] = element;
+     // this.frontIndex = this.endIndex === 0 && this.frontIndex === 0 ? -1 : this.frontIndex;
+     this.endIndex++;
+     this.size++;
+   }
+ 
+   // Remove and return the element from the front of the deque
+   removeFront() {
+     if (this.isEmpty()) {
+       return null;
+     }
+     const front = this.items[this.frontIndex + 1];
+     delete this.items[this.frontIndex + 1];
+     this.frontIndex++;
+     this.size--;
+     return front;
+   }
+ 
+   // Remove and return the element from the rear of the deque
+   removeRear() {
+     if (this.isEmpty()) {
+       return null;
+     }
+     const end = this.items[this.endIndex - 1];
+     delete this.items[this.endIndex - 1];
+     this.endIndex--;
+     this.size--;
+     return end;  
+   }
+ 
+   // Return the element at the front of the deque without removing it
+   peekFront() {
+     if (this.isEmpty()) {
+       return null;
+     }
+     return this.items[this.frontIndex + 1];
+   }
+ 
+   // Return the element at the rear of the deque without removing it
+   peekRear() {
+     if (this.isEmpty()) {
+       return null;
+     }
+     return this.items[this.endIndex - 1];
+   }
+ 
+   // Check if the deque is empty
+   isEmpty() {
+     return this.size === 0;
+   }
+ 
+   // Return the size of the deque
+ 
+   // Clear the deque
+   clear() {
+     this.items = {};
+     this.frontIndex = 0;
+     this.endIndex = 0;
+     this.size = 0;
+   }
+ }
+ 
+ /**
+  * @param {Node} node
+  * @return {Node}
+  */
+ var cloneGraph = function(node) {
+   if (node === null) return null;
+   let nodeMap = {}; // to store all new nodes reference
+   let visited = new Set();
+   let q = new Deque();
+ 
+   nodeMap[node.val] = new Node(node.val, []);
+ 
+   for (let neighbor of node.neighbors) {
+     nodeMap[neighbor.val] = new Node(neighbor.val, []);
+     nodeMap[node.val].neighbors.push(nodeMap[neighbor.val]);
+     q.addFront(neighbor);
+   }
+   visited.add(node.val);
+ 
+   while (q.size > 0) {
+     const curNode = q.removeRear();
+     if (visited.has(curNode.val)) continue;
+     nodeMap[curNode.val] = nodeMap[curNode.val] ? nodeMap[curNode.val] : new Node(curNode.val, []);
+ 
+     for (let neighbor of curNode.neighbors) {
+       nodeMap[neighbor.val] = nodeMap[neighbor.val] ? nodeMap[neighbor.val] : new Node(neighbor.val, []);
+       nodeMap[curNode.val].neighbors.push(nodeMap[neighbor.val]);
+       if (!visited.has(neighbor.val)) {
+         q.addFront(neighbor);
+       }
+     }
+     visited.add(curNode.val);
+   }
+   return nodeMap[1];
+ };
+ let n4 = new  Node(4,[])
+ let n3 = new  Node(3,[])
+ let n2 = new  Node(2,[])
+ let n1 = new  Node(1,[])
+ n4.neighbors.push(n1); n4.neighbors.push(n3);
+ n3.neighbors.push(n2); n3.neighbors.push(n4);
+ n2.neighbors.push(n1); n2.neighbors.push(n3);
+ n1.neighbors.push(n2); n1.neighbors.push(n4);
+ 
+ console.log('RESULT: ', cloneGraph(n1));
+ 
--- main.js
+++ main.js
@@ -1,52 +1,94 @@
 /*
-  Leetcode: 200. Number of Islands (medium) 
+  133. Clone Graph (medium)
 
-  Given an `m x n` 2D binary grid `grid` which represents a map of `'1'`s (land) and `'0'`s (water), return the number of islands.
+  Given a reference of a node in a connected undirected graph.
 
-  An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+  Return a deep copy (clone) of the graph.
+
+  Each node in the graph contains a value (int) and a list (List[Node]) of its neighbors.
+
+  class Node {
+      public int val;
+      public List<Node> neighbors;
+  }
+  
+
+  Test case format:
+
+  For simplicity, each node's value is the same as the node's index (1-indexed). For example, the first node with `val == 1`, the second node with `val == 2`, and so on. The graph is represented in the test case using an adjacency list.
+
+  An adjacency list is a collection of unordered lists used to represent a finite graph. Each list describes the set of neighbors of a node in the graph.
+
+  The given node will always be the first node with `val = 1`. You must return the copy of the given node as a reference to the cloned graph.
 
   
 
   Example 1:
-    Input: grid = [
-      ["1","1","1","1","0"],
-      ["1","1","0","1","0"],
-      ["1","1","0","0","0"],
-      ["0","0","0","0","0"]
-    ]
-    Output: 1
+                          1--------------------2
+                          |   you can't return |
+               |-------x  |    the same graph  |
+               |          4--------------------3
+               |
+    1--------------------2                1-----------------------------2
+    |    original        |   -------->    |    This looks like a        |
+    |     graph          |                |    clone. The nodes are     |
+    4--------------------3                |  New. Graph looks the same  |
+              |                           4-----------------------------3
+              |
+              |             1---------------------------3              
+              |             |   The nodes were          |
+              |--------x    |   cloned. But the graph   |
+                            |   is messed up. Doesn't   |
+                            |    have same connections. |         
+                            4---------------------------2
+
+    Input: adjList = [[2,4],[1,3],[2,4],[1,3]]
+    Output: [[2,4],[1,3],[2,4],[1,3]]
+    Explanation: There are 4 nodes in the graph.
+      1st node (val = 1)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+      2nd node (val = 2)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
+      3rd node (val = 3)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+      4th node (val = 4)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
 
   Example 2:
-    Input: grid = [
-      ["1","1","0","0","0"],
-      ["1","1","0","0","0"],
-      ["0","0","1","0","0"],
-      ["0","0","0","1","1"]
-    ]
-    Output: 3
+
+
+    Input: adjList = [[]]
+    Output: [[]]
+    Explanation: Note that the input contains one empty list. The graph consists of only one node with val = 1 and it does not have any neighbors.
+
+  Example 3:
+    Input: adjList = []
+    Output: []
+    Explanation: This an empty graph, it does not have any nodes.
   
 
   Constraints:
-    - m == grid.length
-    - n == grid[i].length
-    - 1 <= m, n <= 300
-    - grid[i][j] is '0' or '1'.
-  
-  Solution by NeetCode:
-    using bfs
+    - The number of nodes in the graph is in the range [0, 100].
+    - 1 <= Node.val <= 100
+    - Node.val is unique for each node.
+    - There are no repeated edges and no self-loops in the graph.
+    - The Graph is connected and all nodes can be visited starting from the given node.
 
-  Leetcode submission: 
-    #1
-    Runtime: 111 ms, 26.36%
-    Memory: 54.1 MB, 20.37%
-    #2 
-    Runtime: 122 ms, 21.60%
-    Memory: 54 MB, 20.58%
+  Solution by myself:
+    using bfs -> deque 
+    hashmap to store all new created nodes reference 
+  
+
+  LeetCode submission: 
+    Runtime: 64 ms, beats 48.9%
+    Memory: 44.2 MB, beats 23.21%
 */
 
-// Needs deque for BFS. Since Javascript doesn't have deque out of the box, I created it.
-// if the items using array, add/remove the front will have time complexity O(n) because it needs to rearrange the indexes.
-// the items using object/hashmap so add/remove front time complexity is O(1).
+// Definition for a Node.
+class Node {
+  constructor(val, neighbors) {
+    this.val = val === undefined ? 0 : val;
+    this.neighbors = neighbors === undefined ? [] : neighbors;
+  }
+};
+
+// self made deque
 class Deque {
   constructor() {
     this.items = {};
@@ -127,99 +169,48 @@ class Deque {
   }
 }
 
+/**
+ * @param {Node} node
+ * @return {Node}
+ */
+var cloneGraph = function(node) {
+  if (node === null) return null;
+  let nodeMap = {}; // to store all new nodes reference
+  let visited = new Set();
+  let q = new Deque();
 
+  nodeMap[node.val] = new Node(node.val, []);
 
-function numIslands(grid) {
-  if (grid.length === 0) return 0;
+  for (let neighbor of node.neighbors) {
+    nodeMap[neighbor.val] = new Node(neighbor.val, []);
+    nodeMap[node.val].neighbors.push(nodeMap[neighbor.val]);
+    q.addFront(neighbor);
+  }
+  visited.add(node.val);
 
-  const rows = grid.length;
-  const cols = grid[0].length;
-  const visit = new Set();
-  let islands = 0;
+  while (q.size > 0) {
+    const curNode = q.removeRear();
+    if (visited.has(curNode.val)) continue;
+    nodeMap[curNode.val] = nodeMap[curNode.val] ? nodeMap[curNode.val] : new Node(curNode.val, []);
 
-  function bfs(r,c) {
-    q = new Deque();
-    visit.add((r * cols) + c);
-    q.addRear([r,c]);
-
-    while (q.size > 0) {
-      const [ row, col ] = q.removeFront();
-      const directions = [ [1,0], [-1,0], [0,1], [0,-1] ];
-      for (let [dr, dc] of directions) {
-        const r = row + dr; const c = col + dc;
-        if (
-          (r >= 0 && r < rows) && 
-          (c >= 0 && c < cols) && 
-          (grid[r][c] === '1') && 
-          (!visit.has( ( r * cols ) + c ))
-        ) {
-          q.addRear([r, c]);
-          visit.add(( r * cols ) + c);
-        }
+    for (let neighbor of curNode.neighbors) {
+      nodeMap[neighbor.val] = nodeMap[neighbor.val] ? nodeMap[neighbor.val] : new Node(neighbor.val, []);
+      nodeMap[curNode.val].neighbors.push(nodeMap[neighbor.val]);
+      if (!visited.has(neighbor.val)) {
+        q.addFront(neighbor);
       }
     }
+    visited.add(curNode.val);
   }
+  return nodeMap[1];
+};
+let n4 = new  Node(4,[])
+let n3 = new  Node(3,[])
+let n2 = new  Node(2,[])
+let n1 = new  Node(1,[])
+n4.neighbors.push(n1); n4.neighbors.push(n3);
+n3.neighbors.push(n2); n3.neighbors.push(n4);
+n2.neighbors.push(n1); n2.neighbors.push(n3);
+n1.neighbors.push(n2); n1.neighbors.push(n4);
 
-  for (let r = 0; r < rows; r++) {
-    for (let c = 0; c < cols; c++) {
-      if (grid[r][c] === '1' && !visit.has((r * cols) + c)) {
-        bfs(r,c);
-        islands++;
-      }
-    }
-  }
-
-  return islands;
-}
-const grid1 = [
-  ["1","1","1","1","0"],
-  ["1","1","0","1","0"],
-  ["1","1","0","0","0"],
-  ["0","0","0","0","0"]
-]; // expect 1
-const grid2 = [
-  ["1","1","0","0","0"],
-  ["1","1","0","0","0"],
-  ["0","0","1","0","0"],
-  ["0","0","0","1","1"]
-]; // expect 3
-const grid3 = [['1']]; // 1
-const grid4 = [['0']]; // 0
-const grid5 = [
-  ['1', '0'],
-  ['0', '1'],
-]; // 2
-const grid6 = [
-  ["1","0","0","1","1","1","0","1","1","0","0","0","0","0","0","0","0","0","0","0"], // 3
-  ["1","0","0","1","1","0","0","1","0","0","0","1","0","1","0","1","0","0","1","0"], // 4 // 7
-  ["0","0","0","1","1","1","1","0","1","0","1","1","0","0","0","0","1","0","1","0"], // 2 // 9
-  ["0","0","0","1","1","0","0","1","0","0","0","1","1","1","0","0","1","0","0","1"], // 2 // 11
-  ["0","0","0","0","0","0","0","1","1","1","0","0","0","0","0","0","0","0","0","0"], // 0 // 11
-  ["1","0","0","0","0","1","0","1","0","1","1","0","0","0","0","0","0","1","0","1"], // 4 // 15
-  ["0","0","0","1","0","0","0","1","0","1","0","1","0","1","0","1","0","1","0","1"], // 4 // 19
-  ["0","0","0","1","0","1","0","0","1","1","0","1","0","1","1","0","1","1","1","0"], // 1 // 20
-  ["0","0","0","0","1","0","0","1","1","0","0","0","0","1","0","0","0","1","0","1"], // 2 // 22
-  ["0","0","1","0","0","1","0","0","0","0","0","1","0","0","1","0","0","0","1","0"], // 5 // 27
-  ["1","0","0","1","0","0","0","0","0","0","0","1","0","0","1","0","1","0","1","0"], // 3 // 30
-  ["0","1","0","0","0","1","0","1","0","1","1","0","1","1","1","0","1","1","0","0"], // 4 // 34 <--- 33
-  ["1","1","0","1","0","0","0","0","1","0","0","0","0","0","0","1","0","0","0","1"], // 4 // 38
-  ["0","1","0","0","1","1","1","0","0","0","1","1","1","1","1","0","1","0","0","0"], // 3 // 41
-  ["0","0","1","1","1","0","0","0","1","1","0","0","0","1","0","1","0","0","0","0"], // 2 // 43
-  ["1","0","0","1","0","1","0","0","0","0","1","0","0","0","1","0","1","0","1","1"], // 6 // 49
-  ["1","0","1","0","0","0","0","0","0","1","0","0","0","1","0","1","0","0","0","0"], // 4 // 53
-  ["0","1","1","0","0","0","1","1","1","0","1","0","1","0","1","1","1","1","0","0"], // 3 // 56
-  ["0","1","0","0","0","0","1","1","0","0","1","0","1","0","0","1","0","0","1","1"], // 1 // 57
-  ["0","0","0","0","0","0","1","1","1","1","0","1","0","0","0","1","1","0","0","0"]  // 1 // 58
-];// 58
-// console.log('sum :', 3+4+2+2+0+4+4+1+2+5+3+4+4+3+2+6+4+3+1+1);
-const grid7 = [
-  ['0','1','0'],
-  ['1','1','1'],
-  ['0','1','0'],
-  ['1','1','1'],
-  ['1','0','1'],
-  ['1','1','1'],
-  ['0','0','1'],
-  ['1','0','1'],
-]; // 2
-console.log('RESULT: ', numIslands(grid5));
+console.log('RESULT: ', cloneGraph(n1));
