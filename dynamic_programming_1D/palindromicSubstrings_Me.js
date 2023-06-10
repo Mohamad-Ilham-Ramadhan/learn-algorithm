8c48e37b3cd148da8592e96236a87ddb64953846
Mohamad-Ilham-Ramadhan <mohamadilhamramadhan@gmail.com>
2023-06-10 14:33:54 +0700

    (1D dynamic programming) leetcode: 647. Palindromic Substrings (medium), new solution by myself using dynamic programming, the runtime is very fast: 49 ms, beats 99.64%

--- dynamic_programming_1D/palindromicSubstrings_Me.js
+++ dynamic_programming_1D/palindromicSubstrings_Me.js
@@ -29,47 +29,136 @@
     #1 (naive solution) O(n^3)
       Runtime: 375 ms, beats 18.22%;
       Memory: 44 MB, beats 41.90%;
+    #2 (using dynamic programming), newCountSubstrings
+      Runtime: 49 ms, beats 99.64%
+      Memory: 44.4 MB, beats 39.31%
 */
 
 // #1 O(n^3)
 function countSubstrings(s) {
-   let result = 0;
-   let start = [];
-   for (let i = 0; i < s.length; i++) {
-     // result++;
-     start.push(i);
-     /*
-       aaabbaaa
-       a a aa a aaa aa b b bb a abba a aabbaa aa a aaabbaaa aaa aa
-     */
-     for (let j = 0; j < start.length; j++) {
-       const substr = s.slice(j, i+1);
-       let l; let r;
-       if ((i - j + 1) % 2 === 0) {
-         l = Math.floor( (i + j) / 2);
-         r = l + 1;
-       } else {
-         l = r = Math.floor( (i + j) / 2);
-       }
-       let isPalindrome = true;
-       while (l >= j && r <= i) {
-         if (s[l] !== s[r]) { 
-           isPalindrome = false;
-           break;
-         }
-         l--; r++;
-       }
-       if (isPalindrome) result++;
-     }
-   }
-   return result;
- }
- const s1 = 'aaa'; // expect: 6
- const s2 = 'abc'; // expect 3
- const s3 = 'aaabbaaa'; // expect: 18
- // a a aa a aaa aa b b bb a abba a aabbaa aa a aaabbaaa aaa aa'
- const start = Date.now();
- console.log('RESULT: ', countSubstrings(s2));
- console.log('RUNTIME: ', Date.now() - start);
- 
- module.exports = countSubstrings;
+  let result = 0;
+  let start = [];
+  for (let i = 0; i < s.length; i++) {
+    // result++;
+    start.push(i);
+    /*
+      aaabbaaa
+      a a aa a aaa aa b b bb a abba a aabbaa aa a aaabbaaa aaa aa
+    */
+    for (let j = 0; j < start.length; j++) {
+      const substr = s.slice(j, i + 1);
+      let l; let r;
+      if ((i - j + 1) % 2 === 0) {
+        l = Math.floor((i + j) / 2);
+        r = l + 1;
+      } else {
+        l = r = Math.floor((i + j) / 2);
+      }
+      let isPalindrome = true;
+      while (l >= j && r <= i) {
+        if (s[l] !== s[r]) {
+          isPalindrome = false;
+          break;
+        }
+        l--; r++;
+      }
+      if (isPalindrome) result++;
+    }
+  }
+  return result;
+}
+const s1 = 'aaa'; // expect: 6
+const s2 = 'abc'; // expect 3
+const s3 = 'aaabbaaa'; // expect: 18
+// a a aa a aaa aa b b bb a abba a aabbaa aa a aaabbaaa aaa aa'
+const s4 = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'; // 1000 length of a. Expect: 500500
+// const start = Date.now();
+// console.log('RESULT: ', countSubstrings(s4));
+// console.log('RUNTIME: ', Date.now() - start);
+// pattern
+// 1(0 + 1) 3(2 + 1) 6(3 + 3) 10(6 + 4) 15(10 + 5) 21(15 + 6) 28(21 + 7) 36(28 + 8) 45(36 + 9)
+function pattern(n) {
+  const map = { 1: 1 }
+  let s = 'aaa';
+  for (let i = 2; i <= n; i++) {
+    map[i] = map[i - 1] + i;
+    s += 'a';
+  }
+  console.log('s', s)
+  return map[n];
+}
+// console.log('pattern: ', pattern(2))
+
+/*
+  axxabyyb
+  a 1
+  xx 3
+  a 1
+  axxa 1
+  b 1
+  yy 3
+  b 1
+  byyb 1
+
+  axxaaxxa
+  prevTwo [a,4]
+  prevOne [x,5]
+  current [a,7]
+  a 1
+  xx 3
+  axxaa 1
+  aa 3
+  xaax 1
+  xxaaxx 1
+  axxaaxxa 1
+  xx 3
+  axxa 1 
+  a 1
+*/
+// using dynamic programming, O(n)
+function newCountSubstrings(s) {
+    let result = 0;
+    let prevTwo = []; // [char, index], prev two char different from current. Example axxa, a at 0 is prevTwo, current is a at 3
+    let prevOne = []; // [char, index], prev one char different from current. Example axxa, x at 2 is prevTwo, current is a at 3
+    let count = 0; // count substring sequence using dynamic programming
+    let countChars = 0; // chars count for example 'a' = 1, 'bb' = 2, 'xxx' = 3
+    for (let i = 0; i <= s.length; i++) {
+      console.log('prevOne', prevOne, 'prevTwo', prevTwo);
+      const c = s[i];
+      if (s[i-1] !== c) {
+        prevTwo = [...prevOne];
+        prevOne = [s[i-1], i-1];
+        console.log('po', prevOne);
+        // current[0] = s[i];
+        result += count;
+        count = 0;
+        countChars = 0;
+
+        // palindromic count. For example axxa = 1, aaxxaa = 1
+        let l = prevTwo[1];
+        let r = i;
+        while (c !== prevOne[0] && s[l] === s[r] && l >= 0 && r < s.length) {
+          console.log('PALINDROMIC', s.slice(l, r + 1));
+          result++;
+          l--;
+          r++;
+        }
+
+      }
+      countChars++;
+      console.log('count before', count);
+      count = count + countChars;
+      console.log('countChars', countChars);
+      console.log('count after', count);
+
+
+      console.log('result', result);
+      console.log('===========');
+    }
+    return result;
+}
+const start = Date.now();
+console.log('RESULT: ', newCountSubstrings('xaaaax'));
+console.log('RUNTIME: ', Date.now() - start);
+
+module.exports = countSubstrings;
--- test/palindromicSubstrings.test.js
+++ test/palindromicSubstrings.test.js
@@ -15,3 +15,51 @@ test('aaacxxc', () => {
 /*
    a aa a aaa aa a c x xx x cxxc c
 */
+test('aabbaa', () => {
+   expect(countSubstrings('aabbaa')).toEqual(11);
+})
+/*
+   aabbaa
+   aa 3
+   bb 3
+   aa 3
+   abba 1
+   aabbaa 1
+
+   total 11
+
+   aabbaa
+   a a aa b b bb a abba a aabbaa aa
+*/
+test('abxxxbayy', () => {
+   expect(countSubstrings('abxxxbayy')).toEqual(15);
+})
+/*
+   abxxxbayy
+   a 1
+   b 1
+   xxx 6
+   b 1
+   a 1
+   yy 3
+   bxxxb 1
+   abxxba 1
+   total: 15
+*/
+test('abxxxbayya', () => {
+   expect(countSubstrings('abxxxbayya')).toEqual(17);
+})
+/*
+   abxxxbayya
+   a 1
+   b 1
+   xxx 6
+   b 1
+   a 1
+   yy 3
+   a 1
+   bxxxb 1
+   abxxba 1
+   ayya 1
+   total: 17
+*/
