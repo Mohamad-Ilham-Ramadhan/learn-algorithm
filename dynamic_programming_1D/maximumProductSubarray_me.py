8a41b15986bd04fa218cc203cbb467090c599846
Mohamad-Ilham-Ramadhan <mohamadilhamramadhan@gmail.com>
2023-06-18 15:34:03 +0700

    (1D dynamic programming) leetcode: 152. Maximum Product Subarray (medium), solution #2 by myself, decease the number of if/else statement inside the loop which result in faster runtime: 84 ms, beats 93.47%

--- dynamic_programming_1D/maximumProductSubarray_me.py
+++ dynamic_programming_1D/maximumProductSubarray_me.py
@@ -29,9 +29,15 @@
         and check max() product in every iteration
     
     leetcode submission:
-        runtime: 129 ms, beats: 5.32%
-        memory: 19.3 MB, beats: 6.74%
+        attempt #1
+            runtime: 129 ms, beats: 5.32%
+            memory: 19.3 MB, beats: 6.74%
+        attempt #2
+            runtime: 84 ms, beats: 93.47%
+            memory: 19.8 MB, beats 6.74%
 '''
+
+# attempt #1
 def maxProduct(nums):
     m = nums[0] # max
     dp = [[] for n in nums]
@@ -66,6 +72,30 @@ def maxProduct(nums):
     # print('dp after', dp)
     return m
 
+# attempt #2, decrease the number of if/else statement which result in faster runtime.
+def maxProduct2(nums):
+    m = nums[0] # max
+    dp = [[] for n in nums]
+    dp[0] = [nums[0]]
+    for i in range(1, len(nums)): 
+        # print('dp', dp)
+        n = nums[i]
+        for d in dp[i-1]:
+            dp[i].append(d * n)
+            
+        if len(dp[i]) == 1:
+            if (dp[i][0] > 0 and n < 0) or (dp[i][0] < 0 and n > 0) or dp[i][0] == 0:
+                dp[i].append(n)
+        
+        # print('dp[i] after loop', dp[i])
+
+        m = max(*dp[i], m)
+
+        if len(dp[i]) == 2 and dp[i][0] == 0: 
+            dp[i] = [dp[i][1]]
+        
+    return m
+
 n1 = [2,3,-2,4] # expect: 6
 n2 = [-2,0,-1]  # expect: 0
 n3 = [-1,2,-1,8] # expect: 16
@@ -73,11 +103,24 @@ n4 = [-2,-3,-1,-5,-10,-6,-7] # expect: 6300
 n5 = [-2,10,2,1,-3] # expect: 120
 n6 = [-3] # expect: -3
 n7 = [2,-5,-2,-4,3] # expect: 24, output: 20
-
-print('result: ', maxProduct(n7))
+n8 = [0,2] # expect: 2, output: 0
+n9 = [0,-4,9,-2] # expect: 72
+n10 = [0,-4,9,-2,9,-1] # expect: 648
+n11 = [1,2,-4,0,8,3] # expect: 24
+# print('result: ', maxProduct(n7))
+print('RESULT : ', maxProduct2(n11))
 '''
+    [1,2,-4,0,8,3]
+    [1] [2] [-8] [0] [8] [24]
+
+    [0,-4,9,-2,9,-1]
+    [0] [-4]
+
+    [0,-4,9,-2]
+    [0] [0,-4] [0, -36] [0, 72]
+
     [2,-5,-2,-4,3]
-    [2] [-10] [20, -2] [-80, 8] [-120, 24]
+    [2] [-10] [20,-2] [-80, 8]
 
     [2,3,-2,4]
     [6] [-12, -2] [-8, 4]
@@ -94,3 +137,6 @@ print('result: ', maxProduct(n7))
     12600
     [-2] [6, -3] [-6, 3] [30, -15] [-300, 150] [1800, -900] [-12600, 6300]
 '''
+arr = [4,5,11,3,7,1,2,3]
+arr2 = [77,999,33]
+print('zero == minus zero', 0 == -0)
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import unittest
-from coba import maxProduct
+import time
+from coba import maxProduct, maxProduct2
 
 n1 = [2,3,-2,4] # expect: 6
 n2 = [-2,0,-1]  # expect: 0
@@ -8,16 +9,33 @@ n4 = [-2,-3,-1,-5,-10,-6,-7] # expect: 6300
 n5 = [-2,10,2,1,-3] # expect: 120
 n6 = [-3] # expect: -3
 n7 = [2,-5,-2,-4,3] # expect: 24, output: 20
+n8 = [0,2] # expect: 2, output: 0
+n9 = [0,-4,9,-2] # expect: 72
+n10 = [0,-4,9,-2,9,-1] # expect: 648
+n11 = [1,2,-4,0,8,3] # expect: 24
 
 class TestCalc(unittest.TestCase):
+    def setUp(self):
+        self.startTime = time.time()
+    
+    def tearDown(self) -> None:
+        t = time.time() - self.startTime
+        print('%s: %.3f' % (self.id(), t))
+
     def test_xxx(self):
-        self.assertEqual(maxProduct(n1), 6) 
-        self.assertEqual(maxProduct(n2), 0) 
-        self.assertEqual(maxProduct(n3), 16) 
-        self.assertEqual(maxProduct(n4), 6300) 
-        self.assertEqual(maxProduct(n5), 120) 
-        self.assertEqual(maxProduct(n6), -3) 
-        self.assertEqual(maxProduct(n7), 24) 
+        # time.sleep(1)
+        self.assertEqual(maxProduct2(n1), 6) 
+        self.assertEqual(maxProduct2(n2), 0) 
+        self.assertEqual(maxProduct2(n3), 16) 
+        self.assertEqual(maxProduct2(n4), 6300) 
+        self.assertEqual(maxProduct2(n5), 120) 
+        self.assertEqual(maxProduct2(n6), -3) 
+        self.assertEqual(maxProduct2(n7), 24) 
+        self.assertEqual(maxProduct2(n8), 2) 
+        self.assertEqual(maxProduct2(n9), 72) 
+        self.assertEqual(maxProduct2(n10), 648) 
+        self.assertEqual(maxProduct2(n11), 24) 
+        
 
 
 if __name__ == "__main__":
