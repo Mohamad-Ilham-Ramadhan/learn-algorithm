515494ad9468be9373ea59d222f09f0c3250c0fa
Mohamad-Ilham-Ramadhan <mohamadilhamramadhan@gmail.com>
2023-06-19 00:57:43 +0700

    (1D dynamic programming) leetcode: decode ways (medium), solution by me, using bottom up, good runtime

--- test.py
+++ test.py
@@ -1,18 +1,24 @@
 import unittest
 import time
-from coba import maxProduct
+from coba import numDecodings
 
-n1 = [2,3,-2,4] # expect: 6
-n2 = [-2,0,-1]  # expect: 0
-n3 = [-1,2,-1,8] # expect: 16
-n4 = [-2,-3,-1,-5,-10,-6,-7] # expect: 6300
-n5 = [-2,10,2,1,-3] # expect: 120
-n6 = [-3] # expect: -3
-n7 = [2,-5,-2,-4,3] # expect: 24, output: 20
-n8 = [0,2] # expect: 2, output: 0
-n9 = [0,-4,9,-2] # expect: 72
-n10 = [0,-4,9,-2,9,-1] # expect: 648
-n11 = [1,2,-4,0,8,3] # expect: 24
+s1 = '12' # expect: 2 -> 'AB' (1 2) or 'L' (12)
+s2 = '226' # expect: 3 -> 'BZ' (2 26) or 'VF' (22 6) or 'BBF' (2 2 6)
+s3 = '06' # expect: 0
+s4 = '1847624' # expect: 4
+s5 = '8473627' # expect: 1
+s6 = '122341' # expect: 5
+s7 = '121021' # expect: 4
+s8 = '2012260' # expect: 0
+s9 = '201226' # expect  5
+s10 = '1212021' # expect: 6
+s11 = '12234192' # expect: 10
+s12 = '1212121212' # expect: 89
+s13 = '12620' # expect: 3
+s14 = '126200121212' # expect: 0
+s15 = '121012101' # expect: 4
+
+ 
 
 class TestCalc(unittest.TestCase):
     def setUp(self):
@@ -23,18 +29,20 @@ class TestCalc(unittest.TestCase):
         print('%s: %.3f' % (self.id(), t))
 
     def test_xxx(self):
-        # time.sleep(1)
-        self.assertEqual(maxProduct(n1), 6) 
-        self.assertEqual(maxProduct(n2), 0) 
-        self.assertEqual(maxProduct(n3), 16) 
-        self.assertEqual(maxProduct(n4), 6300) 
-        self.assertEqual(maxProduct(n5), 120) 
-        self.assertEqual(maxProduct(n6), -3) 
-        self.assertEqual(maxProduct(n7), 24) 
-        self.assertEqual(maxProduct(n8), 2) 
-        self.assertEqual(maxProduct(n9), 72) 
-        self.assertEqual(maxProduct(n10), 648) 
-        self.assertEqual(maxProduct(n11), 24) 
+        self.assertEqual(numDecodings(s1), 2) 
+        self.assertEqual(numDecodings(s2), 3) 
+        self.assertEqual(numDecodings(s3), 0) 
+        self.assertEqual(numDecodings(s4), 4) 
+        self.assertEqual(numDecodings(s5), 1) 
+        self.assertEqual(numDecodings(s6), 5) 
+        self.assertEqual(numDecodings(s8), 0) 
+        self.assertEqual(numDecodings(s9), 5) 
+        self.assertEqual(numDecodings(s10), 6) 
+        self.assertEqual(numDecodings(s11), 10) 
+        self.assertEqual(numDecodings(s12), 89) 
+        self.assertEqual(numDecodings(s13), 3) 
+        self.assertEqual(numDecodings(s14), 0) 
+        self.assertEqual(numDecodings(s15), 4) 
         
 
 
