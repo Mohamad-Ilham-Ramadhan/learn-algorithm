ef69c6e086ef54f447d237c287a065ab7ab399ff
Mohamad-Ilham-Ramadhan <mohamadilhamramadhan@gmail.com>
2023-06-18 13:09:25 +0700

    (1D dynamic programming) leetcode: 152. Maximum Product Subarray (medium), solution by myself, slow runtime maybe because too much if and elseif statement

--- test.py
+++ test.py
@@ -1,70 +1,23 @@
 import unittest
-from coba import wordBreak
+from coba import maxProduct
 
-s1 = "leetcode"
-wd1 = ["leet", "code"]
-# expect: true
-
-s2 = "applepenapple"
-wd2 = ["apple", "pen"]
-# expect: true
-x2 = "applepenapple"
-xd2 = ["apple", "ap", "pp", "pen"]
-"""
-ap pl
-cat and cats
-cats and cat
-"""
-s3 = "catsandog"
-wd3 = ["cats", "dog", "sand", "and", "cat"]
-# expect: false
-
-s4 = "catandcats"
-wd4 = ["cat", "and", "cats"]
-# expect: true
-s5 = "catsandcat"
-wd5 = ["cat", "and", "cats"]
-# expect: true
-s6 = "ab"
-wd6 = ["a", "b"]  # expect: true
-s7 = "bb"
-wd7 = ["a", "b", "bb", "bbb", "bbbb"]  # expect: true, output: false
-s8 = "cars"
-wd8 = ["car", "ca", "rs"]  # expect: true
-s9 = "a"
-wd9 = ["b"]
-# expect: false,
-s10 = "aaaaaaa"
-wd10 = ["aaaa", "aa"]
-# expect: false, output: true
-s11 = "aaaaaaa"
-wd11 = ["aaaa", "aaa"]  # expect: true, output: false
-s12 = "catscat"
-wd12 = ["cat", "cats"]
-s13 = 'ccbb'
-wd13 = ['bc', 'cb'] # expet: false
-s14 = "catsandogcat"
-wd14 = ["cats","dog","sand","and","cat","an"] # expect: True, output: false
-s15 = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab'
-wd15 = ["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"]
+n1 = [2,3,-2,4] # expect: 6
+n2 = [-2,0,-1]  # expect: 0
+n3 = [-1,2,-1,8] # expect: 16
+n4 = [-2,-3,-1,-5,-10,-6,-7] # expect: 6300
+n5 = [-2,10,2,1,-3] # expect: 120
+n6 = [-3] # expect: -3
+n7 = [2,-5,-2,-4,3] # expect: 24, output: 20
 
 class TestCalc(unittest.TestCase):
     def test_xxx(self):
-        self.assertEqual(wordBreak(s1, wd1), True)  # true
-        self.assertEqual(wordBreak(s2, wd2), True)  # true
-        self.assertEqual(wordBreak(x2, xd2), True)  # true
-        self.assertEqual(wordBreak(s3, wd3), False)  # False
-        self.assertEqual(wordBreak(s4, wd4), True)  # True
-        self.assertEqual(wordBreak(s5, wd5), True)  # True
-        self.assertEqual(wordBreak(s6, wd6), True)  # True
-        self.assertEqual(wordBreak(s8, wd8), True)  # True
-        self.assertEqual(wordBreak(s9, wd9), False)  # False
-        self.assertEqual(wordBreak(s10, wd10), False)  # False
-        self.assertEqual(wordBreak(s11, wd11), True)  # True
-        self.assertEqual(wordBreak(s12, wd12), True)  # True
-        self.assertEqual(wordBreak(s13, wd13), False)  # False
-        self.assertEqual(wordBreak(s14, wd14), True)  # True
-        self.assertEqual(wordBreak(s15, wd15), False)  # False
+        self.assertEqual(maxProduct(n1), 6) 
+        self.assertEqual(maxProduct(n2), 0) 
+        self.assertEqual(maxProduct(n3), 16) 
+        self.assertEqual(maxProduct(n4), 6300) 
+        self.assertEqual(maxProduct(n5), 120) 
+        self.assertEqual(maxProduct(n6), -3) 
+        self.assertEqual(maxProduct(n7), 24) 
 
 
 if __name__ == "__main__":
