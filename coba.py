2d26c7722dc6db3f50f5df9e02687aa97d1fcac0
Mohamad-Ilham-Ramadhan <mohamadilhamramadhan@gmail.com>
2023-06-26 12:18:41 +0700

    (stack) leetcode: 22. Generate Parentheses (medium), soluton #3 by myself, faster than 2 others.

--- coba.py
+++ coba.py
@@ -30,6 +30,10 @@
       #2 (hashmap power up)
          - runtime: 57 ms, beats 21.55%
          - memory: 16.7 MB, beats 60.97%
+      #3 (hashmap power up and is statement order revamped)
+         - runtime: 46 ms, beats 81.43%
+         - memory: 16.6 MB, beats 60.94%
+
 """
 
 
@@ -115,12 +119,63 @@ def solution2(n):
    dfs("", 0, 0, "o")
    return res
 
+# hashmap power up (if statement order revamped)
+def solution3(n):
+   mo = {}  # map of open parentheses
+   mc = {}  # map of close parentheses
+
+   def dfs(s, o, c, operation):
+      print('============')
+      #   print("mo", mo, "mc", mc)
+      if len(s) == n * 2:
+         res.append(s)
+         return
+      if operation == "o":
+         for i in range(1, (n - o) + 1):
+            po = ""
+            if i in mo: 
+               print('hash map cheat!', i)
+               dfs(s + mo[i], o + i, c, "c")
+               continue
+            if i - 1 in mo: 
+               print('Prev open parenthes exist dont loop just append one', i - 1, i, i in mo)
+               mo[i] = mo[i-1] + '('
+               dfs(s + mo[i], o + i, c, "c")
+               continue
+            for j in range(1, i + 1):
+               print('open manual')
+               po += "("
+            mo[j] = po
+            dfs(s + po, o + j, c, "c")
+      else:
+         for i in range(1, (o - c) + 1):
+            pc = ""
+            if i in mc: 
+               print('hash map cheat!', i)
+               dfs(s + mc[i], o, c + i, "o")
+               continue
+            if i - 1 in mc: 
+               print('Prev close parenthes exist dont loop just append one', i - 1, i, i in mc)
+               mc[i] = mc[i-1] + ')'
+               dfs(s + mc[i], o, c + i, "o")
+               continue
+            for j in range(1, i + 1):
+               print('close manual')
+               pc += ")"
+            mc[j] = pc
+            dfs(s + pc, o, c + j, "o")
+
+   res = []
+   dfs("", 0, 0, "o")
+   return res
+
+
 
 n1 = 1
 n2 = 2
 n3 = 3
 n4 = 4
-print("RESULT : ", solution2(5))
+print("RESULT : ", solution3(4))
 
 """
    n = 4
