b919e65ac261df6e1144d8542f13afff5732280c
Mohamad-Ilham-Ramadhan <mohamadilhamramadhan@gmail.com>
2023-06-29 19:03:41 +0700

    (binary search) leetcode: 704. Binary Search (easy), solution by myself, good runtime.

--- write.py
+++ write.py
@@ -1,6 +1,6 @@
 f = open('content.txt', 'w')
 f.write('[')
 import random
-for i in range(10000, -1, -1):
-   f.write(str(random.randint(0,100)) + ',')
+for i in range(0, 10000):
+   f.write(str(i) + ',')
 f.write(']')
