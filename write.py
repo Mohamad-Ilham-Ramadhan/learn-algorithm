cb4012ad1baca6aeca6e6e370b021fa1bd329f06
Mohamad-Ilham-Ramadhan <mohamadilhamramadhan@gmail.com>
2023-07-02 15:37:22 +0700

    (binary search) leetcode: 875. Koko Eatings Bananas (medium), solution by myself, slow runtime only beats 5.28%

--- write.py
+++ write.py
@@ -2,5 +2,16 @@ f = open('content.txt', 'w')
 f.write('[')
 import random
 for i in range(0, 10000):
-   f.write(str(i) + ',')
+   limit = 1
+   if i < 100: 
+      limit = 100
+   elif i < 1000: 
+      limit = 1000 
+   elif i < 3000: 
+      limit = 10000
+   elif i < 9000: 
+      limit = 99999
+   else: 
+      limit =999999999
+   f.write(str(random.randint(1, limit)) + ',')
 f.write(']')
