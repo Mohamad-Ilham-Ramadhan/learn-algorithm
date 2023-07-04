8d0d29a5da0d5b342394612d724507a5aaba13b9
Mohamad-Ilham-Ramadhan <mohamadilhamramadhan@gmail.com>
2023-07-05 00:44:18 +0700

    (binary search)leetcode: 4. medians of two sorted arrays (hard), solution by neetcode, clean and fast.

--- write.py
+++ write.py
@@ -1,17 +1,6 @@
 f = open('content.txt', 'w')
 f.write('[')
 import random
-for i in range(0, 10000):
-   limit = 1
-   if i < 100: 
-      limit = 100
-   elif i < 1000: 
-      limit = 1000 
-   elif i < 3000: 
-      limit = 10000
-   elif i < 9000: 
-      limit = 99999
-   else: 
-      limit =999999999
-   f.write(str(random.randint(1, limit)) + ',')
+for i in range(0, 5):
+   f.write(str(random.randint(1, 100)) + ',')
 f.write(']')
