1ba0b4ea773caa7a0d8504ac5bc80b41dbf7c3f8
Mohamad-Ilham-Ramadhan <mohamadilhamramadhan@gmail.com>
2023-07-08 16:11:47 +0700

    (sliding window) leetcode: 239. Sliding window maximum (hard), solution by myself: monotonic queue and binary search. Slow runtime only beats 5%.

--- write.py
+++ write.py
@@ -1,6 +1,6 @@
 f = open('content.txt', 'w')
-f.write('[')
 import random
-for i in range(0, 5):
-   f.write(str(random.randint(1, 100)) + ',')
+f.write('[')
+for i in range(15): 
+   f.write(str(random.randint(-10,20)) + ',')
 f.write(']')
