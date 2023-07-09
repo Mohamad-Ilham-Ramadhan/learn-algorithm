e678855b13daee14008a8380380560bb43fba1af
Mohamad-Ilham-Ramadhan <mohamadilhamramadhan@gmail.com>
2023-07-09 19:18:06 +0700

    (heap) leetcode: 621. task scheduler (medium), solution by myself, good runtime.

--- write.py
+++ write.py
@@ -1,6 +1,6 @@
 f = open('content.txt', 'w')
 import random
 f.write('[')
-for i in range(15): 
-   f.write(str(random.randint(-10,20)) + ',')
+for i in range(10): 
+   f.write(str(random.randint(-10,10)) + ',')
 f.write(']')
