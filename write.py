89a286184dcb47ddcaa2321b4b996351b9ae1530
Mohamad-Ilham-Ramadhan <mohamadilhamramadhan@gmail.com>
2023-07-11 22:08:04 +0700

    (backtracking): leetcode 78. Subsets (medium), solution by myself, good runtime.

--- write.py
+++ write.py
@@ -1,6 +1,6 @@
 f = open('content.txt', 'w')
 import random
-f.write('[')
-for i in range(11): 
-   f.write(str(random.randint(-5,10)) + ',')
-f.write(']')
+# f.write('[')
+for i in range(10000): 
+   f.write(str(random.randint(0,10000)) + ' ')
+# f.write(']')
