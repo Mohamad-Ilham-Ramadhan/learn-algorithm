2d3edc7efb406975207a5a3776db29f7ff4b5728
Mohamad-Ilham-Ramadhan <mohamadilhamramadhan@gmail.com>
2023-07-17 19:36:41 +0700

    (two pointers) leetcode: 3Sum closest (medium), solution by myself (c++), good runtime.

--- two_pointer/3SumClosest_Me.py
+++ two_pointer/3SumClosest_Me.py
@@ -1,41 +1,41 @@
 ''' 
-    (two pointers) leetcode: 16. 3Sum Closest (medium)
+   (two pointers) leetcode: 16. 3Sum Closest (medium)
 
-    Given an integer array `nums` of length `n` and an integer `target`, find three integers in `nums` such that the sum is closest to `target`.
+   Given an integer array `nums` of length `n` and an integer `target`, find three integers in `nums` such that the sum is closest to `target`.
 
-    Return the sum of the three integers.
+   Return the sum of the three integers.
 
-    You may assume that each input would have exactly one solution.
+   You may assume that each input would have exactly one solution.
 
- 
 
-    Example 1:
 
-        Input: nums = [-1,2,1,-4], target = 1
-        Output: 2
-        Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
-        
-    Example 2:
+   Example 1:
 
-        Input: nums = [0,0,0], target = 1
-        Output: 0
-        Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
-    
+      Input: nums = [-1,2,1,-4], target = 1
+      Output: 2
+      Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+      
+   Example 2:
 
-    Constraints:
-        3 <= nums.length <= 500
-        -1000 <= nums[i] <= 1000
-        -10^4 <= target <= 10^4
-    
-    Related topics: 
-        (array) (two pointers) (sorting)
-    
-    Solution by myself
-        sort
-        two pointers
-    leetcode submission: 
-        runtime: 898 ms, beats 62.58%
-        memory: 16.27 MB, beats 98.40%
+      Input: nums = [0,0,0], target = 1
+      Output: 0
+      Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
+   
+
+   Constraints:
+      3 <= nums.length <= 500
+      -1000 <= nums[i] <= 1000
+      -10^4 <= target <= 10^4
+   
+   Related topics: 
+      (array) (two pointers) (sorting)
+   
+   Solution by myself
+      sort
+      two pointers
+   leetcode submission: 
+      runtime: 898 ms, beats 62.58%
+      memory: 16.27 MB, beats 98.40%
 '''
 # fail
 def threeSumClosest(nums, target): 
