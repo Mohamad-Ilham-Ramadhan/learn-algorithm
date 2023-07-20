8b5af67ce142edf6ebb8e6030d4044c779acdc46
Mohamad-Ilham-Ramadhan <mohamadilhamramadhan@gmail.com>
2023-07-20 18:58:51 +0700

    (linked list) leecode: 234. Palindrome Linked List (easy), space: O(1) using hare and turtle algorithm. GOod runtime.

--- linked_list/palindromeLinkedList_Me.py
+++ linked_list/palindromeLinkedList_Me.py
@@ -1,9 +1,7 @@
 '''
    (linked list) leetcode: 234. Palindrome Linked List (easy)
 
-   Given the head of a singly linked list, return true if it is a 
-palindrome
- or false otherwise.
+   Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
 
  
 
@@ -31,11 +29,22 @@ palindrome
 
    =======================================================================
    Solution by myself:
+      #1
+         but firstly, we store all listnode value to an array/list
+         using two pointers to check is it a palindrome
+      #2
+         using two pointers (hare and turtle), the first is slow, the second is fast (it is two step next)
+         reverse the part two (mid to end) using slow as the first next value for part two
+         then check is it palindrome by using two pointer, the first is point to the head argument, and the second is the part two.next because part two is None initialy.
+         
 
    Leetcode submission:
       #1 time O(n), space O(n)
-      runtime: 927 ms, beats 10.54%
-      memory: 49.52 MB,  beats 9.31%
+         runtime: 927 ms, beats 10.54%
+         memory: beats 9.31%
+      #2 time O(n), space O(1)
+         runtime: 801 ms, beats 47.78%
+         memory: 41.44 MB, beats 66.97%
 
 '''
 # Definition for singly-linked list.
@@ -43,7 +52,7 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-        
+
 # 1
 def isPalindrome(head): 
    nums = []
@@ -64,3 +73,38 @@ def isPalindrome(head):
       l -= 1
       r += 1
    return True
+
+# 2 time: O(n) and space O(1)
+def isPalindrome(head):
+    slow = head
+    fast = head.next
+    while fast: 
+        print('slow', slow.val)
+        print('fast', fast.val)
+        slow = slow.next 
+        if not fast.next or not fast.next.next:
+            break
+        fast = fast.next.next
+    partTwo = ListNode(None);
+    next = slow 
+    while next: 
+        temp = next.next
+        next.next = partTwo.next 
+        partTwo.next = next
+        next = temp
+    
+    print('check palindrome')
+    partTwo = partTwo.next
+    while partTwo: 
+        if head.val !=  partTwo.val: 
+            return False 
+        
+        partTwo = partTwo.next
+        head = head.next  
+    return True
+'''
+   1->2->2->3->2->2->1
+
+   1->2->2->3->2->2->1
+   1->2->2-> 2->2->1
+'''
