4ba68e3825e77b07a8d5480b6d79a600faa11011
Mohamad-Ilham-Ramadhan <mohamadilhamramadhan@gmail.com>
2023-07-30 22:10:16 +0700

    (1D dynamic programming) codeforces: C. Vitamins (1200), solution by myself (python), using bitmask and brute force.

--- codeforce.py
+++ codeforce.py
@@ -1,111 +1,158 @@
 '''
-   (1D Dynamic Programming) Codeforces: round 605 (div 3), C. Yet Another Broken Keyboard (1200)
+6
+10 A
+9 BC
+11 CA
+4 A
+5 B
+3 C
 
-   time limit per test2 seconds
-   memory limit per test256 megabytes
-   input: standard input
-   output: standard output
-
-
-   Recently, Norge found a string s=s1s2…sn consisting of n lowercase Latin letters. As an exercise to improve his typing speed, he decided to type all substrings of the string s. Yes, all n*(n+1)/2 of them!
-
-   A substring of s is a non-empty string x=s[a…b]=sasa+1…sb (1 ≤ a ≤ b ≤ n). For example, "auto" and "ton" are substrings of "automaton".
-
-   Shortly after the start of the exercise, Norge realized that his keyboard was broken, namely, he could use only k Latin letters c1,c2,…,ck out of 26.
-
-   After that, Norge became interested in how many substrings of the string she could still type using his broken keyboard. Help him to find this number.
-
-   Input
-   The first line contains two space-separated integers n and k
-   (1 ≤ n ≤ 2*10^5, 1 ≤ k ≤ 26 ) — the length of the string s and the number of Latin letters still available on the keyboard.
-
-   The second line contains the string s consisting of exactly n lowercase Latin letters.
-
-   The third line contains k space-separated distinct lowercase Latin letters c1,c2,…,ck — the letters still available on the keyboard.
-
-   Output
-   Print a single number — the number of substrings of s that can be typed using only available letters c1,c2,…,ck.
-
-   Examples
-   input
-      7 2
-      abacaba
-      a b
-
-      output: 12
-
-   input
-      10 3
-      sadfaasdda
-      f a d
-
-      output: 21
-
-   input
-      7 1
-      aaaaaaa
-      b
-
-      output: 0
-   Note
-   In the first example Norge can print substrings s[1…2], s[2…3], s[1…3], s[1…1], s[2…2], s[3…3], s[5…6], s[6…7], s[5…7], s[5…5], s[6…6], s[7…7].
-
-   Tags: (combinatorics) (dp) (implementation)
-====================================================
-   Solution by Colin Galen 
-
-   submission: 
-      runtime: 62 ms, memory: 5000 KB
+output: 15
 '''
 
-# [n,k] = input().split(' ')
-# s = input()
-# letters = set(input().split(' '))
+# O(n^3) solution
+# n = int(input())
+# juicesMask = [0] * n
+# juicesPrice = [0] * n
+# for i in range(n):
+#    [price, vits] = input().split(' ')
+#    juicesPrice[i] = int(price)
+#    # A = 0, B = 10, C = 100 (binary)
+#    for vit in vits:
+#       bit = ord(vit) - ord('A')
+#       juicesMask[i] += pow(2, bit)
 
-# input #1 
-# n,k = 7,2
-# s = 'abacaba'
-# letters = set(['a', 'b']) # 12
+# ans = float('inf')
+# # O( (n^3)/3 + (n/3) ) -> simplified to O(n^3) but faster, if n = 6, O(n^3) == 36 but the O( (n^3)/3 + (n/3) ) == 21
+# x = 0
+# for i in range(n):
+#    x += 1
+#    price1 = juicesPrice[i]
+#    if juicesMask[i] == 7:
+#       ans = min(ans, price1)
 
-# input #2
-# n,k = 10,3
-# s = 'sadfaasdda'
-# letters = set(['f', 'a', 'd']) # # 21
+#    for j in range(i+1, n):
+#       x += 1
+#       if juicesMask[i] | juicesMask[j] == juicesMask[i]: # same vitamin
+#          continue
+#       price2 = juicesPrice[j]
+#       if (juicesMask[i] | juicesMask[j]) == 7:
+#          ans = min(ans, price1 + price2)
 
-# input #3
-# n,k = 7,1
-# s = 'aaaaaaa'
-# letters = set(['b']) # 0
+#       for k in range(j+1,n):
+#          x += 1
+#          price3 = juicesPrice[k]
+#          print('total', price1 + price2 + price3)
+#          if (juicesMask[i] | juicesMask[j] | juicesMask[k]) == 7:
+#             ans = min(ans, price1 + price2 + price3)
+            
 
-# input #4
-# n,k = 1,1
-# s = 'a'
-# letters = set(['a']) # 1
 
-[n,k] = input().split(' ')
-n, k = int(n), int(k)
-s = input()
-letters = input().split(' ')
+# print('ans', ans, 'x', x)
+'''
+   dp[1] = {a: inf, b: inf, c: 5}
+   dp[2] = {a: inf, b: 6, c: 5}, 
+   dp[3] = {a: 16, b: 6, c: 5}, ans = 16
+   dp[4] = {a: 4, b: 6, c: 5}, ans = 15
+'''
+# my dynamic programming bitmask approach
+# n = int(input())
+# juicesMask = [0] * n
+# juicesPrice = {'A': float('inf'), 'B': float('inf'), 'C': float('inf')} # {a: min(price), b: min(price), c: min(price)}
+# for i in range(n):
+#    [price, vits] = input().split(' ')
+#    # A = 0, B = 10, C = 100 (binary)
+#    for vit in vits:
+#       bit = ord(vit) - ord('A')
+#       juicesMask[i] += pow(2, bit)
 
-template_array_size = 1e6 + 4265
-a = [0] * len(s)
-typeable = [0] * 26
+# ans = float('inf')
+'''
+   {
+      0: inf, # 
+      1: 4, # A
+      2: 6, # B
+      3: inf, # AB
+      4: 5, # C
+      5: inf, # CA
+      6: inf, # CB
+      7: 16, # ABC
+   }
+'''
 
-for c in letters: 
-   typeable[ord(c) - ord('a')] = 1
+n = int(input())
+inf = float('inf')
+mask = [inf] * 8 # 8 because we use 3 combination 'ABC' 2^3 = 8
 
 for i in range(n):
-   if typeable[ord(s[i]) - ord('a')]:
-      a[i] = 1
+   [price, vits] = input().split(' ')
+   index = 0
+   for vit in vits: 
+      bit = pow(2, ord(vit) - ord('A')) # if vit = 'C' then pow(2, 2) == 4, if vit = 'A' then pow(2,0) == 1
+      index += bit 
+   if int(price) < mask[index]: # update value(price) on the mask 
+      mask[index] = int(price)
 
-f = [0] * (n+1)
-ans = 0
-for i in range(n):
-   if a[i] == 0:
-      f[i+1] = 0
-   else: 
-      f[i+1] = f[i] + 1
+def solve(mask):
+   x = 0
+   ans = float('inf')
+   for i in range(1,8): # i = bit
+      x += 1
+      if i == 7: 
+         # print('try i', mask[i])
+         ans = min(ans, mask[i])
+      for j in range(i+1, 8):
+         x += 1
+         # print(mask[i], mask[j], (i | j), ans)
+         if (i | j) == 7:
+            # print('try j', mask[i] + mask[j])
+            ans = min(ans, mask[i] + mask[j])
+            continue
+         for k in range(j+1, 8):
+            x += 1
+            if(i | j | k) == 7:
+               # print('try k', i,j,k, mask[i] + mask[j] + mask[k])
+               ans = min(ans, mask[i] + mask[j] + mask[k])
+
+   print(ans if ans < float('inf') else -1)
+
+solve(mask)
+
+# all input to maks for testing
+inf = float('inf')
+mask1 = [0, 4,6,inf,5,inf,inf,16] # index as bit and value as price
+mask2 = [0,inf,inf,10,inf,inf,inf,inf]
+mask3 = [0,4,5,inf,inf,11,9,inf]
+mask4 = [0,100,180,inf,inf,160,150,355]
+mask5 = [0,inf,inf,5,inf,inf,11,inf]
+mask6 = [0,3,4,2,16,7,9,1]
+
+# Colin Galen Solution
+# n = int(input())
+# f = [[0] * 8 for i in range(n+1)]
+# inf = 1e17
+# for i in range(n+1):
+#    for j in range(8):
+#       f[i][j] = inf
+
+# f[0][0] = 0
+
+# for i in range(n):
+#    [cost, s] = input().split(' ')
+#    cost = int(cost)
+#    string_mask = 0
+#    for pos in range(3):
+#       c = chr(ord('C') - 0)
+#       have = 0
+#       for d in s:
+#          if c == d:
+#             have = 1
+#       if have: 
+#          string_mask += 1 << pos
    
-   ans += f[i + 1]
+#    for mask in range(8):
+#       f[i+1][mask] = min(f[i+1][mask], f[i][mask])
+#       f[i+1][mask] = 0
+# ans = inf 
 
-print(ans)
+# Let's take a problem, given a set, count how many subsets have sum of elements greater than or equal to a given value.
---
+++ dynamic_programming_1D/vitamins_Me.py
@@ -0,0 +1,140 @@
+'''
+   (1D dynamic programming) codeforces C. Vitamins (1200)
+
+   time limit per test2 seconds
+   memory limit per test256 megabytes
+   inputstandard input
+   outputstandard output
+
+   Berland shop sells n kinds of juices. Each juice has its price c. Each juice includes some set of vitamins in it. There are three types of vitamins: vitamin "A", vitamin "B" and vitamin "C". Each juice can contain one, two or all three types of vitamins in it.
+
+   Petya knows that he needs all three types of vitamins to stay healthy. What is the minimum total price of juices that Petya has to buy to obtain all three vitamins? Petya obtains some vitamin if he buys at least one juice containing it and drinks it.
+
+   Input
+      The first line contains a single integer n (1 ≤ n ≤ 1000) — the number of juices.
+
+      Each of the next n lines contains an integer ci (1 ≤ ci ≤ 100000) and a string si — the price of the i-th juice and the vitamins it contains. String si contains from 1 to 3 characters, and the only possible characters are "A", "B" and "C". It is guaranteed that each letter appears no more than once in each string si. The order of letters in strings si is arbitrary.
+
+Output
+   Print -1 if there is no way to obtain all three vitamins. Otherwise print the minimum total price of juices that Petya has to buy to obtain all three vitamins.
+
+Examples
+   input #1
+      4
+      5 C
+      6 B
+      16 BAC
+      4 A
+      
+      output 15
+
+   input #1
+      2
+      10 AB
+      15 BA
+
+      output -1
+      
+   input #1
+      5
+      10 A
+      9 BC
+      11 CA
+      4 A
+      5 B
+
+      output 13
+
+   input #1
+      6
+      100 A
+      355 BCA
+      150 BC
+      160 AC
+      180 B
+      190 CA
+
+      output 250
+
+   input #1
+      2
+      5 BA
+      11 CB
+      
+      output 16
+
+   Note
+      In the first example Petya buys the first, the second and the fourth juice. He spends 5+6+4=15 and obtains all three vitamins. He can also buy just the third juice and obtain three vitamins, but its cost is 1, which isn't optimal.
+
+      In the second example Petya can't obtain all three vitamins, as no juice contains vitamin "C".
+
+   Tags:
+      (bitmask) (brute force) (dynamic programming) (implementation)
+
+   =============================================================================
+
+   submission:
+      runtime: 93 ms, memory: 3572 KB
+'''
+
+n = int(input())
+inf = float('inf')
+mask = [inf] * 8 # 8 because we use 3 combination 'ABC' 2^3 = 8. [index(bit): value(price)]
+'''
+mask:
+{
+   0: inf, # 
+   1: inf, # A    0001
+   2: inf, # B    0010
+   3: inf, # AB   0011
+   4: inf, # C    0100
+   5: inf, # CA   0101
+   6: inf, # CB   0110
+   7: inf, # ABC  0111
+}
+'''
+
+# collecting bitmask and update the price if it's lower
+for i in range(n):
+   [price, vits] = input().split(' ')
+   index = 0
+   for vit in vits: 
+      bit = pow(2, ord(vit) - ord('A')) # if vit = 'C' then pow(2, 2) == 4, if vit = 'A' then pow(2,0) == 1
+      index += bit 
+   if int(price) < mask[index]: # update value(price) on the mask 
+      mask[index] = int(price)
+
+# brute force
+def solve(mask):
+   x = 0
+   ans = float('inf')
+   for i in range(1,8): # i = bit
+      x += 1
+      if i == 7: 
+         # print('try i', mask[i])
+         ans = min(ans, mask[i])
+      for j in range(i+1, 8):
+         x += 1
+         # print(mask[i], mask[j], (i | j), ans)
+         if (i | j) == 7:
+            # print('try j', mask[i] + mask[j])
+            ans = min(ans, mask[i] + mask[j])
+            continue
+         for k in range(j+1, 8):
+            x += 1
+            if(i | j | k) == 7:
+               # print('try k', i,j,k, mask[i] + mask[j] + mask[k])
+               ans = min(ans, mask[i] + mask[j] + mask[k])
+
+   print(ans if ans < float('inf') else -1)
+
+solve(mask)
+
+# all input to maks for testing
+inf = float('inf')
+mask1 = [0, 4,6,inf,5,inf,inf,16] # index as bit and value as price
+mask2 = [0,inf,inf,10,inf,inf,inf,inf]
+mask3 = [0,4,5,inf,inf,11,9,inf]
+mask4 = [0,100,180,inf,inf,160,150,355]
+mask5 = [0,inf,inf,5,inf,inf,11,inf]
+mask6 = [0,3,4,2,16,7,9,1]
