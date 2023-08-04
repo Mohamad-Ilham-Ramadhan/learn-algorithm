c0e44928045b8a04a828baacc1d098eec331f451
Mohamad-Ilham-Ramadhan <mohamadilhamramadhan@gmail.com>
2023-08-04 23:57:01 +0700

    (bitmask) codeforces: Hello 2019: B. Petr and a Combination Lock (1200), solution by Adhis K.

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,4 +1,23 @@
-auto [key, value] : graph) {
-         cout << "key " << key << "\n";
-         // cout << "value " << value << "\n";
-      }
+ n;
+   // cin >> n;
+
+   // vector<int>a(n);
+   // for (int i = 0; i < n; i++) cin >> a[i];
+
+   // for (int mask = 0; mask < (1 << n); mask++) {
+   //    cout << "mask : " << mask << "\n";
+   //    int sum = 0;
+   //    for (int i = 0; i < n; i++ ) {
+   //       if (mask & (1 << i) ) {
+   //          sum += a[i];
+   //       } else {
+   //          sum -= a[i];
+   //       }
+   //       cout << "sum: " << sum << "\n";
+   //    }
+   //    if (sum % 360 == 0) {
+   //       cout << "YES";
+   //       return 0;
+   //    }
+   // }
+   // cout << "NO";
