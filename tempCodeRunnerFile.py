34534debc1dd8a7681dfcdb3de9b4e36729791d9
Mohamad-Ilham-Ramadhan <mohamadilhamramadhan@gmail.com>
2023-08-07 15:13:28 +0700

    (1D dynamic programming) codeforces 534B Covered Path (1400), solution by myself (c++), two times faster than colin galen and 0 KB memory.

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,19 @@
-
-x += pow(2, 5)
+dp = [0] * (t - 1)
+for i in range(t-1): 
+    result += v1 + bonus 
+    dp[i] = v1 + bonus
+    bonus += d
+result += v2
+# print('result', result)
+# print('dp', dp)
+lastSpeed = v2 
+for i in range(len(dp) - 1, -1, -1): 
+    diff = abs(lastSpeed - dp[i]) - d 
+    # print('diff', diff)
+    if diff > 0: 
+        result -= diff
+        dp[i] -= diff
+    lastSpeed = dp[i]
+# print('result', result)
+# print('dp', dp)
+print(result)
