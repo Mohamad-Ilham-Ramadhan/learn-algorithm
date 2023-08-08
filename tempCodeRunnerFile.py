06ef60f3891b96cad35828e510e93a56de8b5e9b
Mohamad-Ilham-Ramadhan <mohamadilhamramadhan@gmail.com>
2023-08-08 12:16:55 +0700

    (2D dynamic programming) codeforces: 987C Three Display (1400), solution by Colin Galen.

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,19 +1,3 @@
-dp = [0] * (t - 1)
-for i in range(t-1): 
-    result += v1 + bonus 
-    dp[i] = v1 + bonus
-    bonus += d
-result += v2
-# print('result', result)
-# print('dp', dp)
-lastSpeed = v2 
-for i in range(len(dp) - 1, -1, -1): 
-    diff = abs(lastSpeed - dp[i]) - d 
-    # print('diff', diff)
-    if diff > 0: 
-        result -= diff
-        dp[i] -= diff
-    lastSpeed = dp[i]
-# print('result', result)
-# print('dp', dp)
-print(result)
+5
+2 4 5 4 10
+40 30 20 10 40 
