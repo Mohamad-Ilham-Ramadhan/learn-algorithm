ec5a742c995ffe4d24c96cf594d47b7c96304522
Mohamad-Ilham-Ramadhan <mohamadilhamramadhan@gmail.com>
2023-08-12 15:41:39 +0700

    (stable sort) leetcode: 300. Longest Increasing Subsequence (medium), sollution by myself (python).

--- dynamic_programming_1D/longestIncreasingSubsequence_Me.py
+++ dynamic_programming_1D/longestIncreasingSubsequence_Me.py
@@ -12,8 +12,14 @@
     ======================================================================
 
     Submissions: 
-        runtime: 2423 ms, beats 63.73%
-        memory: 16.82 MB, beats 10.41%
+        # dynamic programming
+            runtime: 2423 ms, beats 63.73%
+            memory: 16.82 MB, beats 10.41%
+        
+        # stable sort
+            runtime: 165 ms, beats 77.32%
+            memory: 16.59, beats 91.64%
+
 
 '''
 
@@ -29,21 +35,47 @@ def lengthOfLIS(nums):
     print('dp', dp)
     return result
 
+def withStableSort(nums): 
+    deckStack = [] # 1D array 
+    for i in range(len(nums)-1, -1, -1): 
+        n = nums[i]
+        isInserted = False
+        for j in range(len(deckStack)):
+            if n >= deckStack[j]: 
+                deckStack[j] = n
+                isInserted = True
+                break
+        if not isInserted: 
+            deckStack.append(n)
+            
+    return len(deckStack)
+
+
 n2 = [0,1,0,3,2,3] # 4
 n3 = [7,7,7,7,7,7,7] # 1
-n4 = [-10,4,0,3,-1,8,-4,-2,3,6] # 5
 n7 = [1,2,6,5,4,3,2,1] # 3
 n1 = [10,9,2,5,3,7,101,18] # 4
 n6 = [1,2,3,4,5,6,1,2,3,7,4,5,8,9] # 9 (1,2,3,4,5,6,7,8,9)
 n5 = [1,2,3,7,4,5,8,9] # 7
-print('RESULT: ', lengthOfLIS(n1))
-'''
-    dp [
-        [9,8,5,4,3,2,1], [7,3,2,1], 
-    ]
-    [9,8,6,5,4]
 
-    [3,7,8,9,7]
+n4 = [-10,4,0,3,-1,8,-4,-2,3,6] # 5
+print('RESULT: ', withStableSort(n4))
+'''
+    Stable Sort
+    6 3 -2 -4 -10
+    8 3 -1
+      4  0
+
+    18  7 3 2
+    101 9 5
+        10
+    9 8 5 4 3 2 1 2 1
+        7 6 5 4 3
+
+
+    9 8 5 4 3 2 1
+        7
+
     dp[
         [[18,1]],
         [[101,1]],
