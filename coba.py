be27b2280df55e275f9560f458fb8a5afc32d3e9
Mohamad-Ilham-Ramadhan <mohamadilhamramadhan@gmail.com>
2023-08-23 17:41:57 +0700

    (2D dynamic programming) leetcode: 115. Distinct Subsequences (hard), solution #2 by myself (python), lower space complexity o(n).

--- coba.py
+++ coba.py
@@ -1,5 +1,5 @@
 """
-	(2D Dynamic Programming) 115. Distinct Subsequences (hard)
+	(1D Dynamic Programming) 115. Distinct Subsequences (hard)
 
 	Link: https://leetcode.com/problems/distinct-subsequences/
     
@@ -11,29 +11,46 @@
 	======================================================================
 
 	Submissions: 
-		NeetCode's solution
-			runtime: 711 ms, beats 26.77%
-			memory: 158.90 MB, beats 25.42%
+		runtime: _ ms, beats 26.77%
+		memory: _ MB, beats 25.42%
 """
+'''
+s1 = 'rabbbit'; t1 = 'rabbit' # 3
+	r a b b b i t
+r  1 1 1 1 1 1 1
+a  0 1 1 1 1 1 1
+b  0 0 1 2 3 3 3
+b  0 0 0 1 3 3 3
+i  0 0 0 0 0 3 3
+t  0 0 0 0 0 0 3
 
+s2 = 'babgbag'; t2 = 'bag' # 5
+	0 b a b g b a g
+0  1 1 1 1 1 1 1 1
+b  0 1 1 2 2 3 3 3
+a  0 0 1 1 1 1 4 4
+g  0 0 0 0 1 1 1 5
+'''
+# 1 row of 2D array, 2D dynamic programming
 def numDistinct(s, t):
-	cache = {}
+	dp = [[]] # 1 row of 2D array
+	dp[0] = [1] * (len(s) + 1)
+	print('dp', dp)
+	for i in range(len(t)):
+		# dp.append([0])
+		newRow = [0]
+		for j in range(len(s)):
+			if t[i] == s[j]:
+				# dp[i + 1].append(dp[i + 1][j] + dp[i][j]) # dp[i + 1][j + 1]
+				newRow.append( newRow[j] + dp[0][j] )
+			else:
+				# dp[i + 1].append( dp[i + 1][j] ) # dp[i + 1][j + 1]
+				newRow.append( newRow[j])
+		dp[0] = newRow 
 
-	def dfs(i, j):
-		if j == len(t):
-			return 1
-		if i == len(s):
-			return 0
-		if (i, j) in cache:
-			return [cache[(i, j)]]
-		
-		if s[i] == t[j]:
-			cache[(i, j)] = dfs(i + 1, j + 1) + dfs(i + 1, j)
-		else: 
-			cache[(i, j)] = dfs(i + 1, j)
-		return cache[(i, j)]
-	
-	return dfs(0, 0)
+	print('dp final', dp)
+	return newRow[len(s)]
+
 s1 = 'rabbbit'; t1 = 'rabbit' # 3
 s2 = 'babgbag'; t2 = 'bag' # 5
 print('RESULT: ', numDistinct(s2, t2))
--- dynamic_programming_2D/distinctSubsequences_Me.py
+++ dynamic_programming_2D/distinctSubsequences_Me.py
@@ -11,8 +11,12 @@
 	======================================================================
 
 	Submissions: 
-		runtime: 348 ms, beats 81.79%
-		memory: 73.80 MB, beats 40.01%
+		#1
+			runtime: 348 ms, beats 81.79%
+			memory: 73.80 MB, beats 40.01%
+		#2
+			runtime: 239 ms, beats 90.22%
+			memory: 16.66 MB, beats 89.95%
 """
 '''
 s1 = 'rabbbit'; t1 = 'rabbit' # 3
@@ -20,9 +24,10 @@ s1 = 'rabbbit'; t1 = 'rabbit' # 3
 r  1 1 1 1 1 1 1
 a  0 1 1 1 1 1 1
 b  0 0 1 2 3 3 3
-b  0 0 0 2 3 3 3
+b  0 0 0 1 3 3 3
 i  0 0 0 0 0 3 3
 t  0 0 0 0 0 0 3
+
 
 s2 = 'babgbag'; t2 = 'bag' # 5
 	0 b a b g b a g
@@ -31,6 +36,7 @@ b  0 1 1 2 2 3 3 3
 a  0 0 1 1 1 1 4 4
 g  0 0 0 0 1 1 1 5
 '''
+# O(n * m) space
 def numDistinct(s, t):
 	dp = [[]]
 	dp[0] = [1] * (len(s) + 1)
@@ -45,6 +51,27 @@ def numDistinct(s, t):
 
 	print('dp final', dp)
 	return dp[len(t)][len(s)]
+
+# 1 row of 2D array, 2D dynamic programming
+# o(n) space
+def numDistinct(s, t):
+	dp = [[]] # 1 row of 2D array
+	dp[0] = [1] * (len(s) + 1)
+	print('dp', dp)
+	for i in range(len(t)):
+		# dp.append([0])
+		newRow = [0]
+		for j in range(len(s)):
+			if t[i] == s[j]:
+				# dp[i + 1].append(dp[i + 1][j] + dp[i][j]) # dp[i + 1][j + 1]
+				newRow.append( newRow[j] + dp[0][j] )
+			else:
+				# dp[i + 1].append( dp[i + 1][j] ) # dp[i + 1][j + 1]
+				newRow.append( newRow[j])
+		dp[0] = newRow 
+
+	print('dp final', dp)
+	return newRow[len(s)]
 s1 = 'rabbbit'; t1 = 'rabbit' # 3
 s2 = 'babgbag'; t2 = 'bag' # 5
 print('RESULT: ', numDistinct(s2, t2))
