1e057d1dbbf303007bbaf0452e620e3031fae0ca
Mohamad-Ilham-Ramadhan <mohamadilhamramadhan@gmail.com>
2023-08-25 15:43:35 +0700

    (2D dynamic programming) leetcode: 309. Best Time to Buy and Sell Stock with Cooldown (medium), solution by myself (python), resolves the stuck problem.

--- write.py
+++ write.py
@@ -1,6 +1,6 @@
 f = open('content.txt', 'w')
 import random
 f.write('[')
-for i in range(15): 
-   f.write(str(random.randint(0,20)) + ',')
+for i in range(200): 
+   f.write(str(random.randint(0,1000)) + ',')
 f.write(']')
